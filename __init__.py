ddec264de3efcd2ab1c8ddfda6aad76b75a635aa
Emanuel Demetrescu <emanuel.demetrescu@gmail.com>
2020-04-03 12:42:41 +0200

    Emtools 1.1.70 - Stenone

--- __init__.py
+++ __init__.py
@@ -22,8 +22,8 @@ bl_info = {
     "name": "EM tools",
     "description": "Blender tools for Extended Matrix",
     "author": "E. Demetrescu",
-    "version": (1, 1, 72),
-    "blender": (2, 81, 0),
+    "version": (1, 1, 70),
+    "blender": (2, 82, 0),
 #     "location": "3D View > Toolbox",
 #    "warning": "This addon is still in development.",
     "wiki_url": "",
@@ -63,6 +63,70 @@ from . import (
 
 from .functions import *
 from bpy.utils import register_class, unregister_class
+
+from . import addon_updater_ops
+
+# demo bare-bones preferences
+@addon_updater_ops.make_annotations
+class EmPreferences(bpy.types.AddonPreferences):
+	bl_idname = __package__
+
+	# addon updater preferences
+
+	auto_check_update : bpy.props.BoolProperty(
+		name="Auto-check for Update",
+		description="If enabled, auto-check for updates using an interval",
+		default=False,
+		)
+	updater_intrval_months : bpy.props.IntProperty(
+		name='Months',
+		description="Number of months between checking for updates",
+		default=0,
+		min=0
+		)
+	updater_intrval_days : bpy.props.IntProperty(
+		name='Days',
+		description="Number of days between checking for updates",
+		default=7,
+		min=0,
+		max=31
+		)
+	updater_intrval_hours : bpy.props.IntProperty(
+		name='Hours',
+		description="Number of hours between checking for updates",
+		default=0,
+		min=0,
+		max=23
+		)
+	updater_intrval_minutes : bpy.props.IntProperty(
+		name='Minutes',
+		description="Number of minutes between checking for updates",
+		default=0,
+		min=0,
+		max=59
+		)
+
+	def draw(self, context):
+		layout = self.layout
+		# col = layout.column() # works best if a column, or even just self.layout
+		mainrow = layout.row()
+		col = mainrow.column()
+
+		# updater draw function
+		# could also pass in col as third arg
+		addon_updater_ops.update_settings_ui(self, context)
+
+		# Alternate draw function, which is more condensed and can be
+		# placed within an existing draw function. Only contains:
+		#   1) check for update/update now buttons
+		#   2) toggle for auto-check (interval will be equal to what is set above)
+		# addon_updater_ops.update_settings_ui_condensed(self, context, col)
+
+		# Adding another column to help show the above condensed ui as one column
+		# col = mainrow.column()
+		# col.scale_y = 2
+		# col.operator("wm.url_open","Open webpage ").url=addon_updater_ops.updater.website
+
 
 class EDGESListItem(bpy.types.PropertyGroup):
        """ Group of properties an item in the list """
@@ -339,10 +403,14 @@ classes = (
     EM_epochs_belonging_ob,
     ExportVars,
     ExportTablesVars,
-    EMviqListErrors
+    EMviqListErrors,
+    EmPreferences,
     )
 
 def register():
+
+       addon_updater_ops.register(bl_info)
+
        for cls in classes:
               bpy.utils.register_class(cls)
        
@@ -432,6 +500,9 @@ def register():
 ######################################################################################################
 
 def unregister():
+
+       addon_updater_ops.unregister(bl_info)
+
        for cls in classes:
               try:
                      bpy.utils.unregister_class(cls)
