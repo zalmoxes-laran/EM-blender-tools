05326dc2794ca61ae4f7d2b9805eea6ae7f00b87
emanuel <emanuel.demetrescu@gmail.com>
2021-09-28 14:34:32 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -79,7 +79,7 @@ class EmPreferences(bpy.types.AddonPreferences):
 	auto_check_update : bpy.props.BoolProperty(
 		name="Auto-check for Update",
 		description="If enabled, auto-check for updates using an interval",
-		default=False,
+		default=False
 		)
 	updater_intrval_months : bpy.props.IntProperty(
 		name='Months',
