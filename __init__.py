c53ed6e027744d3d207af5d5eae40b5d2913fc6c
zalmoxes-laran <emanuel.demetrescu@gmail.com>
2022-07-29 16:09:20 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ bl_info = {
     "name": "EM tools",
     "description": "Blender tools for Extended Matrix",
     "author": "E. Demetrescu",
-    "version": (1, 3, 00),
+    "version": (1, 3, 01),
     "blender": (3, 2, 0),
 #     "location": "3D View > Toolbox",
     "warning": "This addon is still in development.",
