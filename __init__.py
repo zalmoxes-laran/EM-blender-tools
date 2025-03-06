fb837b7f019c514ad066b3b3db31bf1eadd5c0f9
Emanuel Demetrescu <emanuel.demetrescu@gmail.com>
2025-03-06 07:39:55 +0100

    updated version

--- __init__.py
+++ __init__.py
@@ -22,9 +22,8 @@ bl_info = {
     "author": "E. Demetrescu",
     "version": (1, 5, 0),  # This is fine as a tuple
     "blender": (4, 0, 0),  # Make sure this matches the minimum Blender version you support
-    #"devel_version": "v1.5.0 dev12",  # This is already a string, which is good
     "location": "3D View > Toolbox",
-    "warning": "This addon is in dev12 stage.",
+    "warning": "This addon is in dev13 stage.",
     "wiki_url": "",
     "category": "Tools",
 }
