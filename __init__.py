cc1c85e736cd2db70548a96125f343d0cf2b7dca
Emanuel Demetrescu <emanuel.demetrescu@gmail.com>
2025-03-20 08:54:17 +0100

    Fixed incoherence in the connection rules of s3Dgraphy

--- __init__.py
+++ __init__.py
@@ -23,7 +23,7 @@ bl_info = {
     "version": (1, 5, 0),  # This is fine as a tuple
     "blender": (4, 0, 0),  # Make sure this matches the minimum Blender version you support
     "location": "3D View > Toolbox",
-    "warning": "1.5.0 dev21",
+    "warning": "1.5.0 dev22",
     "wiki_url": "",
     "category": "Tools",
 }
