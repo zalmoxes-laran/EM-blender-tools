5ff56ee511a0415749468b618e6e8c3f1f95ba8f
Emanuel Demetrescu <emanuel.demetrescu@gmail.com>
2025-05-14 08:59:16 +0200

    test 3 switch to production mode for release

--- __init__.py
+++ __init__.py
@@ -21,7 +21,7 @@ bl_info = {
     "author": "E. Demetrescu",
     "version": (1, 5, 0),
     "blender": (4, 0, 0),
-    "warning": "1.5.0-dev.35",
+    "warning": "1.5.0-dev.36",
     "category": "Tools",
 }
 
